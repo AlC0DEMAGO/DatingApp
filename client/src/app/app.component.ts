dcf774ff217b66fbcf45b9ff2b3435a179097250
AlC0DEMAGO <cesaralatorre22@gmail.com>
2024-09-05 07:08:48 -0600

    commit extra

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'Date me';
+  http = inject(HttpClient);
+  users : any;
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+      next:(response) => {this.users = response},
+      error:(error) => {console.log(error)},
+      complete:() => {console.log("Request completed!")}
+
+    });
+
+
+    
+  }
 }
