0d48253d2f26c882b0532cacaa90a691199243d7
AlC0DEMAGO <cesaralatorre22@gmail.com>
2024-10-03 07:21:33 -0600

    Parical01 - Child to Parent Communication

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,4 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.interfaces;
-using API.services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
