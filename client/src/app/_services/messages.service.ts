52793008e8a92e82cf4f1896cfa56086d69a28f2
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-04-08 21:50:46 -0600

    Parcial05 - Add message thread to SPA

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -6,20 +6,24 @@ import { Message } from '../_models/message';
 import { PaginatedResult } from '../_models/pagination';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class MessagesService {
-    baseUrl = environment.apiUrl;
-    private http = inject(HttpClient);
-    paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
 
-    getMessages(pageNumber: number, pageSize: number, container: string) {
-        let params = setPaginationHeaders(pageNumber, pageSize);
-        params = params.append("Container", container);
+  getMessages(pageNumber: number, pageSize: number, container: string) {
+    let params = setPaginationHeaders(pageNumber, pageSize);
+    params = params.append("Container", container);
 
-        return this.http.get<Message[]>(this.baseUrl + "messages",
-            { observe: "response", params }).subscribe({
-                next: response => setPaginationResponse(response, this.paginatedResult)
-            });
-    }
+    return this.http.get<Message[]>(this.baseUrl + "messages",
+      { observe: "response", params }).subscribe({
+        next: response => setPaginationResponse(response, this.paginatedResult)
+      });
+  }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + "message/thread/" + username);
+  }
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -16,7 +16,7 @@ import { PaginationModule } from 'ngx-bootstrap/pagination';
 })
 export class MessagesComponent implements OnInit {
   messagesService = inject(MessagesService);
-  container = "unread";
+  container = "Inbox"; 
   pageNumber = 1;
   pageSize = 10;
 
@@ -32,9 +32,9 @@ export class MessagesComponent implements OnInit {
   // if (this.container === "outbox") return `/members/${message.recipientUsername}`;
   // else return `/members/${message.senderUsername}`;
 
-  return this.container === "outbox"
-    ? `/members/${message.recipientUsername}`
-    : `/members/${message.senderUsername}`;
+  return this.container === "Outbox"
+      ? `/members/${message.recipientUsername}`
+      : `/members/${message.senderUsername}`;
   }
 
   pageChanged(event: any) {
