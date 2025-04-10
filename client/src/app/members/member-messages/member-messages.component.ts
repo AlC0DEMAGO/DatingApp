ef4172b004b4538c6ab2e6804b69de0e57d96d88
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-04-09 18:43:00 -0600

    Parcial05 - Add Activated Tab

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,27 +1,20 @@
 import { Component, inject, input, OnInit } from '@angular/core';
- import { MessagesService } from '../../_services/messages.service';
- import { Message } from '../../_models/message';
- import { TimeagoModule } from 'ngx-timeago';
+import { MessagesService } from '../../_services/messages.service';
+import { Message } from '../../_models/message';
+import { TimeagoModule } from 'ngx-timeago';
 
- @Component({
-   selector: 'app-member-messages',
-   standalone: true,
-   imports: [TimeagoModule],
-   templateUrl: './member-messages.component.html',
-   styleUrl: './member-messages.component.css'
- })
- export class MemberMessagesComponent implements OnInit {
-   private messagesService = inject(MessagesService);
-   username = input.required<string>();
-   messages: Message[] = [];
- 
-   ngOnInit(): void {
-     this.loadMessages();
-   }
- 
-   loadMessages() {
-     this.messagesService.getMessageThread(this.username()).subscribe({
-       next: messages => this.messages = messages
-     });
-   }
- }
+@Component({
+  selector: 'app-member-messages',
+  standalone: true,
+  imports: [TimeagoModule],
+  templateUrl: './member-messages.component.html',
+  styleUrl: './member-messages.component.css'
+})
+export class MemberMessagesComponent implements OnInit {
+  private messagesService = inject(MessagesService);
+  username = input.required<string>();
+  messages = input.required<Message[]>();
+
+  ngOnInit(): void {
+  }
+}
