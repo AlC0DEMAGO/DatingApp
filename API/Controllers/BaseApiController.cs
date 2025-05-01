f631052076a7005530d0014f31cdee7fabfefc5d
ComandoJKL <92186106+AlC0DEMAGO@users.noreply.github.com>
2025-05-01 15:07:01 -0600

    Parcial05 - Merge to develop (#6)
    * Parcial05 - Add pagination classes
    
    * Parcial05 - Add pagination implementation in controller
    
    * Parcial05 - Add client pagination Part 1
    
    * Parcial05 - Add client pagination Part 2
    
    * Parcial05 - Add initial setup for more filters
    
    * Parcial05 - Add additional filters
    
    * Parcial05 - Clean up member service
    
    * Parcial05 - Add filter buttons to SPA
    
    * Parcial05 - Add sort by to the API
    
    * Parcial05 - Add sort by to the SPA
    
    * Parcial05 - Add ActionFilter
    
    * Parcial05 - Optimize action filter
    
    * Parcial05 - Add TimeAgo module for date formatting
    
    * Parcial05 - Restore members cache
    
    * Parcial05 - Restore cache for member details and persisted filters in members service
    
    * Parcial05 - Add UserLike migration
    
    * Parcial05 - Add LikesRepository
    
    * Parcial05 - Add Likes Controller
    
    * Parcial05 - Add Like feature in SPA
    
    * Parcial05 - Add like toogle feature
    
    * Parcial05 - Add list component to SPA
    
    * Parcial05 - Add pagination for Likes
    
    * Parcial05 - Add likes pagination to SPA
    
    * Parcial05 - Add Message EF Migration
    
    * Parcial05 - Add Message Repository
    
    * Parcial05 - Add AutoMapperProfile for message response
    
    * Parcial05 - Add Message Controller
    
    * Parcial05 - Add Get Messages for User
    
    * Parcial05 - Add Message Thread to controller
    
    * Parcial05 - Add MessagesService to SPA
    
    * Parcial05 - Add Message Inbox 1 of 2
    
    * Parcial05 - Add message inbox
    
    * Parcial05 - Add message thread to SPA
    
    * Parcial05 - Fix to messageS/thread, in PLURAL, see messageS service line 27
    
    * Parcial05 - Add style to messages
    
    * Parcial05 - Add Activated Tab
    
    * Parcial05 - Add query params for tabs
    
    * Parcial02 - Add Route resolver
    
    * Parcial05 - Send messages
    
    * Parcial05 - Fix some small bugs
    
    * Parcial05 - Add deletion of messages in API
    
    * Parcial05 - Add deletion of message in SPA

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
-
+using API.Helpers;
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")]
 public class BaseApiController : ControllerBase
