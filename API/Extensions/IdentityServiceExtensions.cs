93e228edb80dbdecb0750b97296cbd3127514fc3
AlC0DEMAGO <cesaralatorre22@gmail.com>
2025-05-01 15:40:17 -0600

    Parcial06 - Update start up class

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,27 +1,33 @@
+namespace API.Extensions;
 using System.Text;
+using API.Data;
+using API.DataEntities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 
-namespace API.Extensions
+public static class IdentityServiceExtensions
 {
-
-    public static class IdentityServiceExtensions
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
     {
-        public static IServiceCollection AddIdentityServices(this IServiceCollection services,IConfiguration config)
-        {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
+        services.AddIdentityCore<AppUser>(opt => opt.Password.RequireNonAlphanumeric = false)
+            .AddRoles<AppRole>()
+            .AddRoleManager<RoleManager<AppRole>>()
+            .AddEntityFrameworkStores<DataContext>();
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var tokenKey = config["TokenKey"] ?? throw new ArgumentException("TokenKey");
+                options.TokenValidationParameters = new TokenValidationParameters
                 {
-                    var tokenKey = config["TokenKey"] ?? throw new ArgumentNullException("Tokenkey");
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false
-                    };
-                });
-                return services;
-        }
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+        return services;
     }
-};
+}
